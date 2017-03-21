a341a324774da2956603ae6084ce032259006a69
Rijumone Choudhuri <rijumone.choudhuri@mantralabsglobal.com>
2017-03-21 16:54:52 +0530

    init comm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Angular';  title = 'Tour of Heroes';
+  hero = 'Windstorm'; }
